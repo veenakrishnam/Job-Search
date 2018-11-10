2e71a24bb818d621280fa9eab3992891baff8179
veenakrishnam <44857640+veenakrishnam@users.noreply.github.com>
2018-11-10 11:52:13 -0500

    Add files via upload

--- jobsearch.sql
+++ jobsearch.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.1.14
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 08, 2018 at 05:29 AM
--- Server version: 10.1.19-MariaDB
--- PHP Version: 5.6.28
+-- Generation Time: Nov 10, 2018 at 05:51 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `jobsearch`
@@ -26,11 +26,12 @@ SET time_zone = "+00:00";
 -- Table structure for table `admin`
 --
 
-CREATE TABLE `admin` (
+CREATE TABLE IF NOT EXISTS `admin` (
   `id` int(20) NOT NULL,
   `username` varchar(30) NOT NULL,
   `password` varchar(30) NOT NULL,
-  `admin_name` varchar(30) NOT NULL
+  `admin_name` varchar(30) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -46,8 +47,8 @@ INSERT INTO `admin` (`id`, `username`, `password`, `admin_name`) VALUES
 -- Table structure for table `company`
 --
 
-CREATE TABLE `company` (
-  `id` int(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `company` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
   `company_name` varchar(100) NOT NULL,
   `website` varchar(100) NOT NULL,
   `city` varchar(100) NOT NULL,
@@ -60,8 +61,9 @@ CREATE TABLE `company` (
   `number` varchar(50) NOT NULL,
   `email` varchar(100) NOT NULL,
   `username` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `password` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `company`
@@ -69,7 +71,7 @@ CREATE TABLE `company` (
 
 INSERT INTO `company` (`id`, `company_name`, `website`, `city`, `state`, `postal_code`, `country`, `companytype`, `firstname`, `lastname`, `number`, `email`, `username`, `password`) VALUES
 (1, 'Dell', 'www.dell.com', 'Bangalore', 'Karnataka', '560092', 'India', 'AutoMobiles', 'Veena', 'M', '098765457', 'veena@gmail.com', 'dell', 'dell'),
-(3, 'google', 'fsdfs', 'knjas', 'dhwe', '', 'Denmark', 'Manufacturing', 'madhu', 'g', '46279', 'madhu@gmail.com', 'madhu', 'madhu');
+(3, 'google', 'fsdfs', 'knjas', 'dhwe', '', 'Denmark', 'Manufacturing', 'madhu', 'g', '46279', 'madhu@gmail.com', 'google', 'google');
 
 -- --------------------------------------------------------
 
@@ -77,8 +79,8 @@ INSERT INTO `company` (`id`, `company_name`, `website`, `city`, `state`, `postal
 -- Table structure for table `job`
 --
 
-CREATE TABLE `job` (
-  `id` int(20) NOT NULL,
+CREATE TABLE IF NOT EXISTS `job` (
+  `id` int(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(100) NOT NULL,
   `position` varchar(100) NOT NULL,
   `qualification` varchar(50) NOT NULL,
@@ -92,8 +94,9 @@ CREATE TABLE `job` (
   `venue` varchar(100) NOT NULL,
   `date_time` varchar(100) NOT NULL,
   `status` varchar(50) NOT NULL,
-  `company_id` varchar(20) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `company_id` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `job`
@@ -101,7 +104,8 @@ CREATE TABLE `job` (
 
 INSERT INTO `job` (`id`, `title`, `position`, `qualification`, `branch`, `job_location`, `batch`, `percentage`, `skills`, `salary`, `process`, `venue`, `date_time`, `status`, `company_id`) VALUES
 (1, 'Campus Recruitement', 'Software Developer', 'be', 'ise', 'Bangalore', 2018, '60', 'C++, JAVA', '3LPA', 'Apti, GD, HR', 'SVCE Campus', 'Friday 19 October 2018 - 09:00', 'active', '1'),
-(4, 'qqqqqqcg', 'SAc', 'B.Tech', 'CIV', 'bbcv', 0, '78', 'dkfsjn', '98765', 'gdfngdkln', 'gjldfd', 'Saturday 20 October 2018 - 13:25', 'active', '3');
+(4, 'qqqqqqcg', 'SAc', 'B.Tech', 'CIV', 'bbcv', 0, '78', 'dkfsjn', '98765', 'gdfngdkln', 'gjldfd', 'Saturday 20 October 2018 - 13:25', 'active', '3'),
+(5, 'dsad', 'dassda', 'BE', 'MECH', 'dasd', 0, '12', 'dsa', 'das', 'das', 'das', 'Wednesday 07 November 2018 - 23:50', 'active', '1');
 
 -- --------------------------------------------------------
 
@@ -109,19 +113,23 @@ INSERT INTO `job` (`id`, `title`, `position`, `qualification`, `branch`, `job_lo
 -- Table structure for table `job_register`
 --
 
-CREATE TABLE `job_register` (
-  `id` int(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `job_register` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
   `company_id` int(50) NOT NULL,
   `job_id` int(50) NOT NULL,
-  `student_id` int(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `student_id` int(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
 
 --
 -- Dumping data for table `job_register`
 --
 
 INSERT INTO `job_register` (`id`, `company_id`, `job_id`, `student_id`) VALUES
-(1, 1, 1, 1);
+(1, 1, 1, 1),
+(7, 3, 4, 1),
+(8, 1, 5, 1),
+(9, 1, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -129,73 +137,24 @@ INSERT INTO `job_register` (`id`, `company_id`, `job_id`, `student_id`) VALUES
 -- Table structure for table `student`
 --
 
-CREATE TABLE `student` (
-  `id` int(50) NOT NULL,
-  `username` varchar(50) NOT NULL,
+CREATE TABLE IF NOT EXISTS `student` (
+  `id` int(50) NOT NULL AUTO_INCREMENT,
   `password` varchar(50) NOT NULL,
-  `student_name` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `student_name` varchar(50) NOT NULL,
+  `student_email` varchar(200) NOT NULL,
+  `student_contact` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `student_email` (`student_email`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
 
 --
 -- Dumping data for table `student`
 --
 
-INSERT INTO `student` (`id`, `username`, `password`, `student_name`) VALUES
-(1, 'madhu', 'madhu', 'madhu');
+INSERT INTO `student` (`id`, `password`, `student_name`, `student_email`, `student_contact`) VALUES
+(1, 'madhu', 'Madhu', 'madhu@gmail.com', ''),
+(2, 'veena123', 'Veena Krishna', 'veena@gmail.com', '123456789');
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `company`
---
-ALTER TABLE `company`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `job`
---
-ALTER TABLE `job`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `job_register`
---
-ALTER TABLE `job_register`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `student`
---
-ALTER TABLE `student`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `company`
---
-ALTER TABLE `company`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
---
--- AUTO_INCREMENT for table `job`
---
-ALTER TABLE `job`
-  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `job_register`
---
-ALTER TABLE `job_register`
-  MODIFY `id` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
